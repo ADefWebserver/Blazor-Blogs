ce33845ed19b3ed7d46d562b94ac93cbf8c0c724
Michael Washington <webmaster@adefwebserver.com>
2019-12-28 14:33:42 -0800

    Registration working

---
+++ BlazorBlogs/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BlazorBlogs.Data;
+using BlazorBlogs.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BlazorBlogs.Areas.Identity.IdentityHostingStartup))]
+namespace BlazorBlogs.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
