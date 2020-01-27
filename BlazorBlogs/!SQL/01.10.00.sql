0125922c97cb888767672f1e2341fa7302b662db
Michael Washington <webmaster@adefwebserver.com>
2020-01-26 17:29:14 -0800

    Added Files Tables to .sql

--- BlazorBlogs/!SQL/01.10.00.sql
+++ BlazorBlogs/!SQL/01.10.00.sql
@@ -142,6 +142,26 @@ IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[
 ALTER TABLE [dbo].[Comment] CHECK CONSTRAINT [FK_Comment_Comment]
 GO
 
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Files]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[Files](
+	[FileId] [int] IDENTITY(1,1) NOT NULL,
+	[FileName] [nvarchar](500) NOT NULL,
+	[FilePath] [nvarchar](4000) NOT NULL,
+	[CreateDate] [datetime] NOT NULL,
+	[DownloadCount] [int] NOT NULL,
+ CONSTRAINT [PK_Files] PRIMARY KEY CLUSTERED 
+(
+	[FileId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+
 SET IDENTITY_INSERT [dbo].[Settings] ON 
 GO
 INSERT [dbo].[Settings] ([SettingID], [SettingName], [SettingValue]) VALUES (1, N'AllowRegistration', N'True')
