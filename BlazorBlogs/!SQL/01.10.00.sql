36a527ab4205cd16d02d8156a72fdc0708d2f4c2
Michael Washington <webmaster@adefwebserver.com>
2020-01-31 06:19:19 -0800

    #22 Starting support for File downloads

--- BlazorBlogs/!SQL/01.10.00.sql
+++ BlazorBlogs/!SQL/01.10.00.sql
@@ -184,7 +184,7 @@ INSERT [dbo].[Settings] ([SettingID], [SettingName], [SettingValue]) VALUES (9, 
 GO
 INSERT [dbo].[Settings] ([SettingID], [SettingName], [SettingValue]) VALUES (10, N'ApplicationLogo', N'uploads\logo.png')
 GO
-INSERT [dbo].[Settings] ([SettingID], [SettingName], [SettingValue]) VALUES (11, N'ApplicationHeader', N'<span>The Ultimate Blogging Platform...</span>')
+INSERT [dbo].[Settings] ([SettingID], [SettingName], [SettingValue]) VALUES (11, N'ApplicationHeader', N'')
 GO
 SET IDENTITY_INSERT [dbo].[Settings] OFF
 GO
---
+++ BlazorBlogs/Controllers/DownloadController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBlogs
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DownloadController : Controller
+    {
+        private readonly IWebHostEnvironment environment;
+        public DownloadController(IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult DownloadFile(string FileName)
+        {
+            string path = Path.Combine(
+                                environment.WebRootPath,
+                                "files",
+                                FileName);
+
+            var stream = new FileStream(path, FileMode.Open);
+
+            var result = new FileStreamResult(stream, "text/plain");
+            result.FileDownloadName = FileName;
+            return result;
+        }
+    }
+}
