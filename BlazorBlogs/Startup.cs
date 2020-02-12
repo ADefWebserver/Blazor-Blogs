be862be2f1d59cc13459859d6891f00f99f2b422
Michael Washington <webmaster@adefwebserver.com>
2020-02-12 05:28:13 -0800

    #29

--- BlazorBlogs/Startup.cs
+++ BlazorBlogs/Startup.cs
@@ -5,6 +5,7 @@ using Blazored.Toast;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
@@ -55,6 +56,7 @@ namespace BlazorBlogs
             services.AddScoped<SearchState>();
             services.AddScoped<DisqusState>();
             services.AddHttpContextAccessor();
+            services.AddScoped<HttpContextAccessor>();
             services.AddBlazoredToast();
             services.AddHeadElementHelper();
         }
