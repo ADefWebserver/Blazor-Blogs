c422ca4c83ba48a3d0a13ed20c30af3dd30f1aea
Michael Washington <webmaster@adefwebserver.com>
2020-02-16 11:48:07 -0800

    For new accounts, set NewsletterSubscriber = true

--- BlazorBlogs/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BlazorBlogs/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -83,7 +83,7 @@ namespace BlazorBlogs.Areas.Identity.Pages.Account
                 ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
                 if (ModelState.IsValid)
                 {
-                    var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                    var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, NewsletterSubscriber = true };
                     var result = await _userManager.CreateAsync(user, Input.Password);
                     if (result.Succeeded)
                     {
