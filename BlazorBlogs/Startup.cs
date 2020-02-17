9737b1af11d6ae4f069126db6eb98b8a907c5a53
Michael Washington <webmaster@adefwebserver.com>
2020-02-16 22:08:21 -0800

    Refactored most of the code into a BlazorBlogsLibrary

--- BlazorBlogs/Startup.cs
+++ BlazorBlogs/Startup.cs
@@ -1,14 +1,5 @@
-using BlazorBlogs.Areas.Identity;
-using BlazorBlogs.Data;
-using BlazorBlogs.Data.Models;
-using Blazored.Toast;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,41 +17,9 @@ namespace BlazorBlogs
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddDbContext<BlazorBlogsContext>(options =>
-            options.UseSqlServer(
-                Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddDefaultIdentity<ApplicationUser>(
-                  options => options.SignIn.RequireConfirmedAccount = true)
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-
-            services.AddRazorPages();
-            services.AddServerSideBlazor()
-                .AddCircuitOptions(options => { options.DetailedErrors = true; });
-
-            services.AddScoped<AuthenticationStateProvider, 
-                RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-
-            services.AddScoped<BlogsService>();
-            services.AddScoped<GeneralSettingsService>();
-            services.AddScoped<EmailService>();
-            services.AddTransient<IEmailSender, EmailSender>();
-            services.AddScoped<SearchState>();
-            services.AddScoped<DisqusState>();
-            services.AddMetaWeblog<MetaWeblogService>();
-            services.AddHttpContextAccessor();
-            services.AddScoped<HttpContextAccessor>();
-            services.AddBlazoredToast();
-            services.AddHeadElementHelper();
+            services.AddBlazorBlogsServices(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ BlazorBlogsLibrary/RegisterServices.cs
@@ -0,0 +1,68 @@
+﻿using BlazorBlogs;
+using BlazorBlogs.Areas.Identity;
+using BlazorBlogs.Data;
+using BlazorBlogs.Data.Models;
+using Blazored.Toast;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+using WilderMinds.MetaWeblog;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class RegisterServices
+    {
+        public static IServiceCollection AddBlazorBlogsServices(
+            this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            if (services is null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<BlazorBlogsContext>(options =>
+            options.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<ApplicationUser>(
+                  options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddRazorPages();
+            services.AddServerSideBlazor()
+                .AddCircuitOptions(options => { options.DetailedErrors = true; });
+
+            services.AddScoped<AuthenticationStateProvider,
+                RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+
+            services.AddScoped<BlogsService>();
+            services.AddScoped<GeneralSettingsService>();
+            services.AddScoped<EmailService>();
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.AddScoped<SearchState>();
+            services.AddScoped<DisqusState>();
+            services.AddMetaWeblog<BlazorBlogs.MetaWeblogService>();
+            services.AddHttpContextAccessor();
+            services.AddScoped<HttpContextAccessor>();
+            services.AddBlazoredToast();
+            services.AddHeadElementHelper();
+
+            return services;
+        }        
+    }
+}
