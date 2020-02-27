43665b8904fc2f81feda7d87f085e0c507e53090
Michael Washington <webmaster@adefwebserver.com>
2020-02-27 07:02:51 -0800

    Upgrade now tested and working in Azure

---
+++ BlazorBlogs/SQLScripts/01.10.00.sql
@@ -0,0 +1,4 @@
+﻿UPDATE [dbo].[Settings]
+   SET [SettingValue] = '01.10.00'
+ WHERE [SettingName] = 'VersionNumber'
+GO
--- BlazorBlogsLibrary/Controllers/RestartApp.cs
+++ BlazorBlogsLibrary/Controllers/RestartApp.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
@@ -12,21 +14,38 @@ namespace BlazorBlogs
     [Authorize(Roles = "Administrators")]
     public class RestartAppController : Controller
     {
-        private IHostApplicationLifetime ApplicationLifetime { get; set; }
-        private IHttpContextAccessor _httpContextAccessor;
+        private readonly IWebHostEnvironment _hostEnvironment;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public RestartAppController(IHostApplicationLifetime appLifetime,
+        public RestartAppController(IWebHostEnvironment environment,
             IHttpContextAccessor httpContextAccessor)
         {
-            ApplicationLifetime = appLifetime;
+            _hostEnvironment = environment;
             _httpContextAccessor = httpContextAccessor;
         }
 
         // api/RestartApp/ShutdownSite
         [HttpGet("[action]")]
-        public ContentResult ShutdownSite(string url)
+        public ContentResult ShutdownSite()
         {
-            ApplicationLifetime.StopApplication();
+            string WebConfigOrginalFileNameAndPath = _hostEnvironment.ContentRootPath + @"\Web.config";
+            string WebConfigTempFileNameAndPath = _hostEnvironment.ContentRootPath + @"\Web.config.txt";
+
+            if (System.IO.File.Exists(WebConfigOrginalFileNameAndPath))
+            {
+                // Temporarily rename the web.config file
+                // to release the locks on any assemblies
+                System.IO.File.Copy(WebConfigOrginalFileNameAndPath, WebConfigTempFileNameAndPath);
+                System.IO.File.Delete(WebConfigOrginalFileNameAndPath);
+
+                // Give the site time to release locks on the assemblies
+                Task.Delay(2000).Wait(); // Wait 2 seconds with blocking
+
+                // Rename the temp web.config file back to web.config
+                // so the site will be active again
+                System.IO.File.Copy(WebConfigTempFileNameAndPath, WebConfigOrginalFileNameAndPath);
+                System.IO.File.Delete(WebConfigTempFileNameAndPath);
+            }
 
             return new ContentResult
             {
