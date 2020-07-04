f5599f45c00c3d0a68b28e210093fc57389ba42a
Michael Washington <Webmaster@ADefWebserver.com>
2020-07-04 10:05:49 -0700

    Installs version 01.00.30

--- BlazorBlogs/SQLScripts/01.00.30.sql
+++ BlazorBlogs/SQLScripts/01.00.30.sql
@@ -17,7 +17,7 @@ CREATE TABLE [dbo].[ExternalConnections](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 END
-GO
+
 UPDATE [dbo].[Settings]
    SET [SettingValue] = '01.00.30'
  WHERE [SettingName] = 'VersionNumber'
