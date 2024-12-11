7de8327263f7b31e5e331f7c23785521d1fb812d
Michael Washington <webmaster@adefwebserver.com>
2024-12-11 09:20:42 -0800

    Project builds

--- BlazorBlogs/Data/Models/ApplicationUser.cs
+++ BlazorBlogs/Data/Models/ApplicationUser.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BlazorBlogs.Data.Models
+namespace BlazorBlogs.Data
 {
     public class ApplicationUser : IdentityUser
     {
--- BlazorBlogs/Program.cs
+++ BlazorBlogs/Program.cs
@@ -1,4 +1,3 @@
-using BlazorBlogs.Areas.Identity;
 using BlazorBlogs.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.Authorization;
@@ -9,6 +8,13 @@ using System.Runtime.Loader;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 using WilderMinds.MetaWeblog;
 
+using BlazorBlogs.Components;
+using BlazorBlogs.Components.Account;
+using Blazored.Toast;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
 namespace BlazorBlogs
 {
     public class Program
@@ -18,28 +24,64 @@ namespace BlazorBlogs
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
             // Read the connection string from the appsettings.json file
             builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddEnvironmentVariables();
 
-            // Get HostingEnvironment
-            var env = builder.Environment;
+            // Allows appsettings.json to be updated programatically
+            builder.Services.ConfigureWritable<ConnectionStrings>(builder.Configuration.GetSection("ConnectionStrings"));
+            builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
 
-            var BlazorBlogsLibraryPath = Path.GetFullPath(@"CustomModules\BlazorBlogsLibrary.dll");
+            builder.Services.AddCascadingAuthenticationState();
+            builder.Services.AddScoped<IdentityUserAccessor>();
+            builder.Services.AddScoped<IdentityRedirectManager>();
+            builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
 
-            var BlazorBlogsAssembly =
-                AssemblyLoadContext
-                .Default.LoadFromAssemblyPath(BlazorBlogsLibraryPath);
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            })
+                .AddIdentityCookies();
 
-            Type BlazorBlogsType =
-                BlazorBlogsAssembly
-                .GetType("Microsoft.Extensions.DependencyInjection.RegisterServices");
-
-            builder.Services.AddMvc(options => options.EnableEndpointRouting = false)
-                .AddApplicationPart(BlazorBlogsAssembly);
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             
-            BlazorBlogsType.GetMethod("AddBlazorBlogsServices")
-                .Invoke(null, new object[] { builder });
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+            builder.Services.AddDbContext<BlazorBlogsContext>(options =>
+            options.UseSqlServer(connectionString));
+
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddIdentityCore<ApplicationUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+            })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddSignInManager()
+                .AddRoleManager<RoleManager<IdentityRole>>()
+                .AddRoleStore<RoleStore<IdentityRole, ApplicationDbContext>>()
+                .AddDefaultTokenProviders();
+
+            builder.Services.AddScoped<BlogsService>();
+            builder.Services.AddScoped<GeneralSettingsService>();
+            builder.Services.AddScoped<EmailService>();
+            builder.Services.AddTransient<IEmailSender, EmailSender>();
+            builder.Services.AddScoped<SearchState>();
+            builder.Services.AddScoped<DisqusState>();
+            builder.Services.AddScoped<InstallUpdateState>();
+            builder.Services.AddMetaWeblog<BlazorBlogs.MetaWeblogService>();
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddScoped<HttpContextAccessor>();
+            builder.Services.AddScoped<HttpClient>();
+            builder.Services.AddBlazoredToast();
+            builder.Services.AddHeadElementHelper();
 
             var app = builder.Build();
 
@@ -60,15 +102,18 @@ namespace BlazorBlogs
 
             app.UseHttpsRedirection();
 
-            app.UseStaticFiles();
+            app.UseAntiforgery();
 
-            app.UseRouting();
+            app.MapStaticAssets();
 
             app.UseAuthorization();
 
-            app.MapControllers();
-            app.MapBlazorHub();
-            app.MapFallbackToPage("/_Host");
+            app.MapStaticAssets();
+            app.MapRazorComponents<Components.App>()
+                .AddInteractiveServerRenderMode();
+
+            // Add additional endpoints required by the Identity /Account Razor components.
+            app.MapAdditionalIdentityEndpoints();
 
             app.Run();
         }
